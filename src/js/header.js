02c128aaa355cfa9a73f7dcd45d45ced15aa16f2
Tauriel1202 <lothlorienexplorer20@gmail.com>
2024-01-24 23:14:47 -0500

    hurricane overview and what added along with pics

--- src/js/header.js
+++ src/js/header.js
@@ -18,23 +18,46 @@ function Header() {
           <h1>Stormwatchers: Kids</h1>
           <div className="imgDiv">
             <a href="/account">
-              <img src="https://github.com/Tauriel1202/stormwatchers/blob/master/public/imgs/icons/accountImg.png?raw=true" alt="account" width="100" height="100" />
+              <img
+                src="https://github.com/Tauriel1202/stormwatchers/blob/master/public/imgs/icons/accountImg.png?raw=true"
+                alt="account"
+                width="100"
+                height="100"
+              />
             </a>
           </div>
+            <button
+              onClick={() => {
+                document.querySelector("nav").classList.toggle("on");
+                this.classList.toggle("on");
+              }}
+            ></button>
         </div>
-        <button onClick={() => {
-          document.querySelector('nav').classList.toggle('on')
-          this.classList.toggle('on')
-        }}></button>
         <nav>
-          <li><a href="/">Home</a></li>
-          <li><a href="/weather">Weather Types</a></li>
-          <li><a href="/stormprep">Storm Prep</a></li>
-          <li><a href="/pastweather">Past Weather Data</a></li>
-          <li><a href="/legendarystorms">Legendary Storms</a></li>
-          <li><a href="/stormwatch">Report a Storm</a></li>
-          <li><a href="/printables">Printable Activities</a></li>
-          <li className="smallNav"><a href="/account">Account</a></li>
+          <li>
+            <a href="/">Home</a>
+          </li>
+          <li>
+            <a href="/weather">Weather Types</a>
+          </li>
+          <li>
+            <a href="/stormprep">Storm Prep</a>
+          </li>
+          <li>
+            <a href="/pastweather">Past Weather Data</a>
+          </li>
+          <li>
+            <a href="/legendarystorms">Legendary Storms</a>
+          </li>
+          <li>
+            <a href="/stormwatch">Report a Storm</a>
+          </li>
+          <li>
+            <a href="/printables">Printable Activities</a>
+          </li>
+          <li className="smallNav">
+            <a href="/account">Account</a>
+          </li>
         </nav>
       </header>
     </>
